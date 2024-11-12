959fbd269937e81fcded96ea148b2cc2ecefe363
Victor <VictorArcium@users.noreply.github.com>
2024-11-12 08:25:27 +0100

    Comments.

--- ff_derive/src/pow_fixed.rs
+++ ff_derive/src/pow_fixed.rs
@@ -15,6 +15,7 @@ pub(crate) fn generate(
     exponent: BigUint,
 ) -> proc_macro2::TokenStream {
     let steps = build_addition_chain(exponent.clone());
+    // last_usage[i] indicates the last usage of the i-th temporary variable.
     let last_usage = steps.iter().enumerate().fold(Vec::new(), |mut acc, (n, step)| {
         acc.push(n);
         match step {
@@ -23,13 +24,21 @@ pub(crate) fn generate(
         }
         acc
     });
-    let mut drops: Vec<(usize, usize)> = last_usage.into_iter().enumerate().collect();
-    drops.sort_by_key(|(_i, last_usage)| *last_usage);
+    // drops is the ordered sequence of possible drops of the temporary variable
+    // the first component is the variable to drop, the second is when to drop it
+    let drops: Vec<(usize, usize)> = {
+        let mut drops: Vec<(usize, usize)> = last_usage.into_iter().enumerate().collect();
+        drops.sort_by_key(|(_i, last_usage)| *last_usage);
+        drops
+    };
 
     let mut gen = proc_macro2::TokenStream::new();
 
+    // contains all idents that can be reused
     let mut free_idents: Vec<Ident> = Vec::new();
+    // the number of variables that have been used so far.
     let mut n_vars = 0usize;
+    // give code for using a free variable, and the corresponding ident
     fn get_free_variable(v: &mut Vec<Ident>, n_vars: &mut usize) -> (proc_macro2::TokenStream, Ident) {
         if let Some(last) = v.pop() {
             (quote! { #last }, last)
@@ -43,12 +52,14 @@ pub(crate) fn generate(
     // First entry in chain is one, i.e. the base.
     let (start_code, start) = get_free_variable(&mut free_idents, &mut n_vars);
     gen.extend(quote! {
-        #start_code = *#base;
+        #start_code = *#base; // the asterisk makes it work
     });
 
     let mut tmps = vec![start];
+    // current location in drops
     let mut drop_index = 0usize;
     for (i, step) in steps.into_iter().enumerate() {
+        // allow reuse of everything that will be dropped in this instruction
         while drop_index < drops.len() && drops[drop_index].1 <= i {
             free_idents.push(tmps[drops[drop_index].0].clone());
             drop_index += 1;
